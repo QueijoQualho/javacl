f0b81fa89fdc56a59c38854155bab986e20292d7
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2023-10-22 03:51:08 -0300

    rename representantes to clientes

--- src/main/java/com/java_cl/classes/pessoa/Representante.java
+++
@@ -1,8 +0,0 @@
-package com.java_cl.classes.pessoa;
-
-public class Representante extends Usuario{
-
-    public Representante(String nome, String telefone, String email, String cpf, String cargo, String senha) {
-        super(nome, telefone, email, cpf, cargo, senha);
-    }
-}
