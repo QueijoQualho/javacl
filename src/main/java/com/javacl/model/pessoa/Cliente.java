f3ae769ee5f047e3edbc6296c7c4f95c4738d64b
Pedro Jesus <Pedromra.04@gmail.com>
2024-03-13 21:20:51 -0300

    update

--- src/main/java/com/javacl/model/pessoa/Cliente.java
+++ src/main/java/com/javacl/model/pessoa/Cliente.java
@@ -5,4 +5,5 @@ public class Cliente extends Usuario{
     public Cliente(String nome, String telefone, String email, String cpf, String cargo, String senha) {
         super(nome, telefone, email, cpf, cargo, senha);
     }
+
 }
