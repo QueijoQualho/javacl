bc42eeaff1aa7bafff179afb39f2a70dad855c20
Pedro Jesus <Pedromora.04@gmail.com>
2024-03-17 17:07:35 -0300

    update

--- src/main/java/com/javacl/repositorys/usuarioRepository/UsuarioRepository.java
+++ src/main/java/com/javacl/repositorys/usuarioRepository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.javacl.repositorys.usuarioRepository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.javacl.model.pessoa.Usuario;
@@ -7,5 +8,5 @@ import com.javacl.model.pessoa.Usuario;
 public abstract class UsuarioRepository {
     
     public abstract List<Usuario> getUsuarios(); 
-    public abstract void saveUsuario(Usuario usuario); 
+    public abstract void saveUsuario(Usuario usuario) throws SQLException; 
 }
